2ed7295ef1bfd08bd89d2f0adb978ee7e044064d
Putrabungsu6 <muhammadnorseputra@gmail.com>
2026-05-28 21:39:24 +0800

    fix: redirect logout

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -62,7 +62,7 @@ export function DashboardHeader({ user }: { user: any }) {
                       return result.message;
                     }
                     window.location.href =
-                      `${process.env.NEXT_PUBLIC_LOGOUT_URL}/redirecting` ||
+                      `${process.env.NEXT_PUBLIC_LOGOUT_URL}/checking` ||
                       "http://localhost:3000";
                     return result.message;
                   },
